1767835d303e41d02770aa88e445b24e0f7de41f
ravi <robbie.mhzn.1@gmail.com>
2024-01-15 20:22:55 +0545

    test

--- tr/main.tf
+++ tr/main.tf
@@ -11,3 +11,24 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
+
+# Define the VPC
+resource "aws_vpc" "testvpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "testvpc"
+  }
+}
+
+
+
+# Define the subnets
+resource "aws_subnet" "sub-us-east-1a" {
+  vpc_id = aws_vpc.testvpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "sub-us-east-1a"
+  }
+}
